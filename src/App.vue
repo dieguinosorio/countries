5dd3bb2d943d5d3a8151a22bae2b0cc382c159c7
DESKTOP-RG46VVH\AUXSISTEMAS <dieguinosorio@gmail.com>
2021-07-30 16:56:03 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Paises></Paises>
+</template>
+<script>
+import Paises from './components/countries/index.vue'
+export default {
+  name: 'App',
+  components: {
+    Paises
+  }
+}
+</script>
---
+++ src/components/countries/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+        <h1>prueba</h1>
+    </div>
+</template>
+<script>
+import axios from 'axios'; 
+export default {
+    data(){
+        return{
+            paises:[],
+            continents:[],
+        }
+    },
+
+    methods:{
+        getPaises(){
+            let me = this;
+            let url ="https://restcountries.eu/rest/v2/all";
+            axios.get(url).then(response=>{
+                response.data.map(function(e){
+                    me.paises.push(e);
+                    if(me.continents.indexOf(e.region) < 0){
+                        me.continents.push(e.region);
+                        console.log(e.region)
+                    }
+                })
+            })
+        },
+    },
+
+    mounted(){
+        this.getPaises();
+    }
+}
+</script>
